8f46aa7dde69574a60e95887a18e71d312b9c527
Ian Rose <ianrose14@gmail.com>
2021-10-04 10:17:39 -0400

    upload.sh fix and add usage hints to index.html

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-gcloud auth login ianrose14@gmail.com
-
 # work around https://github.com/GoogleCloudPlatform/gsutil/issues/1118
 export CLOUDSDK_PYTHON=python2.7
+
+gcloud auth login ianrose14@gmail.com
 
 gsutil cp index.html gs://georgia-maps/
 gsutil -m cp districts/*.kmz gs://georgia-maps/geo/
